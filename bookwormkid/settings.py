905a6c7665fc1382c32e793ef2bc40e8b1f843b1
Lucia Ferencik <lferencikova@gmail.com>
2023-09-05 13:06:13 +0000

    Add Home app and templates

--- bookwormkid/settings.py
+++ bookwormkid/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # Installed apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -62,12 +65,15 @@ ROOT_URLCONF = 'bookwormkid.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
