8eaea8c9f0969d85041d65174563e71a00d731da
Lucia Ferencik <lferencikova@gmail.com>
2023-09-12 07:21:01 +0000

    Small changes/clean up +create  products.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Product
 
-# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
