994553e30bc31c203c9cffa35738ebf93f3db0b8
Lucia Ferencik <lferencikova@gmail.com>
2023-09-25 12:32:58 +0000

    Add delete product functionality

--- products/views.py
+++ products/views.py
@@ -165,3 +165,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store offer """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product was deleted.')
+    return redirect(reverse('products'))
