11e55dd13fd35332bc0154111539cdc747480ec4
Lucia Ferencik <lferencikova@gmail.com>
2023-10-02 11:44:33 +0000

    Set up Aricle admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'author', 'status', 'created_on')
+    search_fields = ['title', 'content', 'author']
