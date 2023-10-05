81af4d793bc781c0e49183faaa7d9b322c0f5788
Lucia Ferencik <lferencikova@gmail.com>
2023-10-05 13:42:12 +0000

    Bug-fix: Update wishlist context processor

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -4,14 +4,12 @@ from .models import Wishlist
 
 def wishlist_contents(request):
     """Wishlist contents context processor"""
-    wishlist_contents = []
+    wishlist_items = []
 
     if request.user.is_authenticated:
         user_profile = UserProfile.objects.get(user=request.user)
         wishlist_items = Wishlist.objects.filter(user_profile=user_profile)
 
-    context = {
+    return {
         'customer_wishlist': wishlist_items,
     }
-
-    return context
