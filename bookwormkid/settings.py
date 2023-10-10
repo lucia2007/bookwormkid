9e1e78941b40e0f5885ea318275474968d424c2d
Lucia Ferencik <lferencikova@gmail.com>
2023-10-10 09:37:12 +0000

    Test commit

--- bookwormkid/settings.py
+++ bookwormkid/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['bookwormkid-c20568b2004e.herokuapp.com', 'localhost', '8000-lucia2007-bookwormkid-kzzr8bsd1y0.ws-eu105.gitpod.io']
 
