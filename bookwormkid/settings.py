934944e050b06aece14f5c5b398d621b04c4f9aa
Lucia Ferencik <lferencikova@gmail.com>
2023-10-10 10:07:52 +0000

    Set debug back to false in deployed

--- bookwormkid/settings.py
+++ bookwormkid/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bookwormkid-c20568b2004e.herokuapp.com', 'localhost', '8000-lucia2007-bookwormkid-kzzr8bsd1y0.ws-eu105.gitpod.io']
 
