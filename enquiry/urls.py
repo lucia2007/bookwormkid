733193c53d7353d25f0dcf342de40dc96c20b763
Lucia Ferencik <lferencikova@gmail.com>
2023-10-12 17:48:49 +0000

    Enquiry add/edit/delete

--- enquiry/urls.py
+++ enquiry/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.all_enquiries, name='enquiries'),
     path('add/', views.add_enquiry, name='add_enquiry'),
+    path('edit/<int:enquiry_id>/', views.edit_enquiry, name='edit_enquiry'),
+    path('delete/<int:enquiry_id>/', views.delete_enquiry, name='delete_enquiry'),
 ]
--- enquiry/views.py
+++ enquiry/views.py
@@ -34,17 +34,12 @@ def add_enquiry(request):
         return redirect(reverse('home'))
 
     enquiries = Enquiry.objects.all()
-    print("I'm a superuser.")
 
     if request.method == 'POST':
         form = EnquiryForm(request.POST)
 
-        print(enquiries)
-
         if form.is_valid():
-            print('inside the form valid function')
             form.save()
-            print('i am saved')
             messages.success(request, "Enquiry was added successfully.")
             return redirect(reverse('enquiries'))
         else:
@@ -62,3 +57,57 @@ def add_enquiry(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_enquiry(request, enquiry_id):
+    """ Edit an enquiry """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owner can edit enquiries.')
+        return redirect(reverse('home'))
+
+    enquiry = get_object_or_404(Enquiry, pk=enquiry_id)
+
+    if request.method == 'POST':
+        form = EnquiryForm(request.POST, instance=enquiry)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Enquiry was edited successfully.")
+            return redirect(reverse('enquiries'))
+        else:
+            messages.error(request,
+                           'Enquiry was not edited. Correct the form inputs.'
+                           )
+    else:
+        form = EnquiryForm(instance=enquiry)
+
+    template = 'enquiry/edit_enquiry.html'
+    context = {
+        'form': form,
+        'enquiry': enquiry,
+        'on_page': True,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_enquiry(request, enquiry_id):
+    """ Delete an enquiry """
+    if not request.user.is_superuser:
+        messages.error(
+            request,
+            'Sorry, only the admin can delete enquiries.')
+        return redirect(reverse('enquiries'))
+
+    enquiry = get_object_or_404(Enquiry, pk=enquiry_id)
+    enquiry.delete()
+    messages.success(request,
+                     f'Enquiry was deleted successfully.')
+
+    context = {
+        'on_page': True,
+    }
+
+    return redirect(reverse('enquiries'), context)
