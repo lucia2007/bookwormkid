a094f92227e30d747e41a44297f4176bdf65143f
Lucia Ferencik <lferencikova@gmail.com>
2023-10-23 06:45:31 +0000

    Add login required decorator to product_bought

--- products/views.py
+++ products/views.py
@@ -107,6 +107,7 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
+@login_required
 def product_bought_by_request_user(request, product_id):
     # Get the product instance
     product = get_object_or_404(Product, pk=product_id)
