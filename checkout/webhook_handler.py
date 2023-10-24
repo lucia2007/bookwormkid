eb994359704686b983125b4c1abae37902d728d9
Lucia Ferencik <lferencikova@gmail.com>
2023-10-24 07:35:20 +0000

    Fix long lines, missing newlines, trailing spaces

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -35,7 +35,7 @@ class StripeWH_Handler:
                 body,
                 settings.DEFAULT_FROM_EMAIL,
                 [cust_email]
-            )   
+            )
         except Exception as e:
             print(e)
             logging.error(e)
@@ -81,8 +81,8 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address1 = shipping_details.address.line1  # noqa
+                profile.default_street_address2 = shipping_details.address.line2  # noqa
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -112,7 +112,8 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                content=f'Webhook received: {event["type"]} | '
+                'SUCCESS: Verified order already in database',
                 status=200)
         else:
             order = None
@@ -141,7 +142,7 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for size, quantity in item_data['items_by_size'].items():  # noqa
                             order_line_item = OrderLineItem(
                                 order=order,
                                 product=product,
@@ -157,7 +158,8 @@ class StripeWH_Handler:
                     status=500)
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | '
+            'SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
--- enquiry/urls.py
+++ enquiry/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.all_enquiries, name='enquiries'),
     path('add/', views.add_enquiry, name='add_enquiry'),
     path('edit/<int:enquiry_id>/', views.edit_enquiry, name='edit_enquiry'),
-    path('delete/<int:enquiry_id>/', views.delete_enquiry, name='delete_enquiry'),
+    path(
+        'delete/<int:enquiry_id>/',
+        views.delete_enquiry,
+        name='delete_enquiry'),
 ]
