a7ead60a74f0d7e0a6da780b7bea8e5f9281f807
Lucia Ferencik <lferencikova@gmail.com>
2023-10-25 09:46:00 +0000

    Remove from Wishlist button on products page

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -4,12 +4,16 @@ from .models import Wishlist
 
 def wishlist_contents(request):
     """Wishlist contents context processor"""
-    wishlist_items = []
 
     if request.user.is_authenticated:
+        wishlist_items = []
         user_profile = UserProfile.objects.get(user=request.user)
-        wishlist_items = Wishlist.objects.filter(user_profile=user_profile)
-
+        wishlist_queryset = Wishlist.objects.filter(user_profile=user_profile)
+        # if there are items in the users wishlist
+        for item in wishlist_queryset:
+            wishlist_items.append(item.product)
+    else:
+        wishlist_items = []
     return {
         'customer_wishlist': wishlist_items,
     }
