cd0632d287d995b9f888b8f3d8649d9b2d1f1f84
Lucia Ferencik <lferencikova@gmail.com>
2023-11-01 14:41:03 +0000

    Only admin can access bookstore management page

--- bookstoremanagement/views.py
+++ bookstoremanagement/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.urls import reverse
+from django.contrib import messages
 
 
+@login_required
 def bookstoremanagement(request):
     """ A view to return the bookstoremanagement page """
 
-    return render(
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only a store admin can access this link.')
+        return redirect(reverse('home'))
+    else:
+        return render(
                   request,
                   'bookstoremanagement/bookstoremanagement.html',
                   )
